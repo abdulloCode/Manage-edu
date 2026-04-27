9c866fd0bbdc2aacba4aaa29f71df857485c6479
abdunozir <ibyvtnq@gmail.com>
2026-04-27 21:28:49 +0500

    Merged with origin/abdullo-new branch § jotai added to packages

--- src/api/groups.js
+++ src/api/groups.js
@@ -1,7 +1,7 @@
 import api from "./axios";
 
 // export const getMyGroups = () => api.get("/groups/me");
-// export const getAllGroups = () => api.get("/groups");
+export const getAllGroups = () => api.get("/groups");
 // export const getGroupById = (id) => api.get(`/groups/${id}`);
 // export const getAllRooms = () => api.get("/rooms");
 // export const updateGroup = (id, data) => api.put(`/groups/${id}`, data);
